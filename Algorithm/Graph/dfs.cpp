3354ac7e08d956794a65ede05e2f22ee987eb478
ivanj26 <ivanjth26@gmail.com>
2020-01-20 15:52:41 +0700

    Added dfs algorithm with recursive method

--- Algorithm/Graph/dfs.cpp
+++ Algorithm/Graph/dfs.cpp
@@ -10,6 +10,17 @@ template <class T>
 class DFSAlgorithm {
     private:
         DirectedGraph<T> g;
+        void dfsUtil(vector<T>* nodes, bool* visited, T s) {
+            visited[(long) s] = true;
+            cout << s << " ";
+
+            for (auto i = nodes[s].begin(); i != nodes[s].end(); i++)
+            {
+                if (!visited[(long) *i]) {
+                    dfsUtil(nodes, visited, *i);
+                }
+            }
+        }
     public:
         DFSAlgorithm(const DirectedGraph<T>& g){
             this->g = g;
@@ -46,18 +57,32 @@ class DFSAlgorithm {
             cout << endl;
             delete[] visited;
         }
+
+        //Recursive method
+        void doFromNode2(T s) {
+            long size = g.nbOfNodes;
+            vector<T> *nodes = g.adjNodes;
+
+            bool *visited = new bool[size];
+            for (long i = 0; i < size; i++)
+            {
+                visited[i] = false;
+            }
+
+            dfsUtil(nodes, visited, s);
+        }
 };
 
 int main(int argc, const char** argv) {
     DirectedGraph<int> g(5);
     g.addAdjNodes(1, 0);
+    g.addAdjNodes(0, 3);
     g.addAdjNodes(0, 2);
     g.addAdjNodes(2, 1);
-    g.addAdjNodes(0, 3);
     g.addAdjNodes(1, 4);
 
     DFSAlgorithm<int> dfs(g);
-    dfs.doFromNode(0);
+    dfs.doFromNode2(0);
 
     return 0;
 }
