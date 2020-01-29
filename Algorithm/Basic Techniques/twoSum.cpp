ddef0caed9581a7ecac9be5ba2d077704dbeefba
ivanj26 <ivanjth26@gmail.com>
2020-01-29 07:16:18 +0700

    Added twoSums coding practice

---
+++ Algorithm/Basic Techniques/twoSum.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        map.insert({nums[i], i});
+    }
+
+    vector<int> res;
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+        unordered_map<int, int>::iterator it = map.find(diff);
+
+        if (it != map.end()) {
+            int idx = it->second;
+            if (idx != i) {
+                res.push_back(i);
+                res.push_back(idx);
+
+                return res;
+            }
+        }
+    }
+
+    return res;
+}
