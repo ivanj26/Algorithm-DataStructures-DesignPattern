1642a88eeb46c891930e650a0ae1edba7ef5c8b9
ivanj26 <ivanjth26@gmail.com>
2021-10-16 10:28:01 +0700

    add alternative algorithm for two sum

--- Algorithm/LeetCode Practices/twoSum.cpp
+++ Algorithm/LeetCode Practices/twoSum.cpp
@@ -32,3 +32,31 @@ vector<int> twoSum(vector<int> &nums, int target)
 
 	return res;
 }
+
+vector<int> twoSum(vector<int> &nums, int target) {
+    vector<int> v;
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+        int diff2 = target + nums[i];
+
+        unordered_map<int, int>::iterator it = map.find(diff);
+        if (it != map.end()) {
+            v.push_back(it->second);
+            v.push_back(i);
+            break;
+        }
+
+        unordered_map<int, int>::iterator it2 = map.find(diff2);
+        if (it2 != map.end()) {
+            v.push_back(it2->second);
+            v.push_back(i);
+            break;
+        }
+
+        map.insert(make_pair(nums[i], i));
+    }
+
+    return v;
+}
