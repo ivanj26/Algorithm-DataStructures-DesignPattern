e307d44a7293eec7e1170cb0fdb76906d3bab6ff
ivanj26 <ivanjth26@gmail.com>
2021-12-11 06:31:26 +0700

    clone graph driver

--- Algorithm/Graph/cloneGraph.cpp
+++ Algorithm/Graph/cloneGraph.cpp
@@ -1,65 +1,98 @@
 #include <unordered_map>
 #include <vector>
-
+#include <iostream>
 class Node {
-    public:
-        int val;
-        std::vector<Node *> neighbors;
+	public:
+		int val;
+		std::vector<Node*> neighbors;
 
-        Node()
-        {
-            val = 0;
-            neighbors = vector<Node *>();
-        }
-        Node(int _val)
-        {
-            val = _val;
-            neighbors = vector<Node *>();
-        }
-        Node(int _val, vector<Node *> _neighbors)
-        {
-            val = _val;
-            neighbors = _neighbors;
-        }
+		Node()
+		{
+			this->val = 0;
+			this->neighbors = std::vector<Node*>();
+		}
+
+		Node(int val)
+		{
+			this->val = val;
+			this->neighbors = std::vector<Node*>();
+		}
+
+		Node(int val, std::vector<Node*> neighbors)
+		{
+			this->val = val;
+			this->neighbors = neighbors;
+		}
 };
 
-class Solution {
-    private:
-        std::unordered_map<int, Node *> map;
+class GraphCloner {
+	private:
+		std::unordered_map<int, Node *> map;
 
-        void cloneUtil(Node* curr)
-        {
-            int size = curr->neighbors.size();
+		void cloneUtil(Node* curr)
+		{
+			int size = curr->neighbors.size();
 
-            for (int i = 0; i < size; i++) {
-                Node* neighbor = curr->neighbors[i];
+			for (int i = 0; i < size; i++)
+			{
+				Node* neighbor = curr->neighbors[i];
 
-                if (map.find(neighbor->val) != map.end())
-                {
-                    curr->neighbors[i] = map[neighbor->val];
-                } else
-                {
-                    curr->neighbors[i] = new Node(neighbor->val, neighbor->neighbors);
-                    map[neighbor->val] = curr->neighbors[i];
+				if (map.find(neighbor->val) != map.end())
+				{
+					curr->neighbors[i] = map[neighbor->val];
+				} else
+				{
+					curr->neighbors[i] = new Node(neighbor->val, neighbor->neighbors);
+					map[neighbor->val] = curr->neighbors[i];
 
-                    cloneUtil(curr->neighbors[i]);
-                }
-            }
-        }
+					this->cloneUtil(curr->neighbors[i]);
+				}
+			}
+		}
 
-    public:
-        Node *cloneGraph(Node *node)
-        {
-            if (node == nullptr)
-            {
-                return node;
-            }
+	public:
+		Node *clone(Node *node)
+		{
+			if (node == nullptr)
+			{
+				return node;
+			}
 
-            Node *clone = new Node(node->val, node->neighbors);
-            map[clone->val] = clone;
+			Node* cloned = new Node(node->val, node->neighbors);
+			map[cloned->val] = cloned;
 
-            cloneUtil(clone);
+			this->cloneUtil(cloned);
 
-            return clone;
-        }
+			return cloned;
+		}
 };
+
+void printGraph(Node* node)
+{
+	if (node) {
+		std::cout << node->val << std::endl;
+		for (Node* n : node->neighbors)
+		{
+			printGraph(n);
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	GraphCloner cloner;
+
+	std::vector<Node*> neighbors;
+	neighbors.push_back(new Node(2));
+	neighbors.push_back(new Node(3));
+	neighbors.push_back(new Node(4));
+
+	Node* node = new Node(1, neighbors);
+	
+	Node* cloned = cloner.clone(node);
+
+	// print the graph and its children
+	printGraph(cloned);
+	
+	return 0;
+}
