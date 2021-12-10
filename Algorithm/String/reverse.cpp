df2bb3d0b9f77303880ca0ff7dfe8fedd22a0d9c
ivanj26 <ivanjth26@gmail.com>
2021-12-11 05:51:47 +0700

    reverse string update code

--- Algorithm/String/reverse.cpp
+++ Algorithm/String/reverse.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 string reverse(string str) {
-	if (str.length() < 2) {
+	if (str.length() == 1) {
 		return str;
 	}
 
-	return reverse(str.substr(1)) + str.at(0);
+	int size = str.size();
+	int lastIdx = size - 1; 
+	return str.at(lastIdx) + reverse(str.substr(0, size - 1));
 }
 
 int main(int argc, char const *argv[])
