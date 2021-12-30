a6334b50dc18df8821d2df8736c645a22640f43b
ivanj26 <ivanjth26@gmail.com>
2021-12-30 17:27:38 +0700

    feat: first not repeating characters 2nd solution

--- Algorithm/LeetCode Practices/firstNotRepeatingCharacter.cpp
+++ Algorithm/LeetCode Practices/firstNotRepeatingCharacter.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <unordered_map>
 
+using namespace std;
+
 char firstNotRepeatingCharacter(std::string s)
 {
 	int len = s.length();
@@ -37,6 +39,43 @@ char firstNotRepeatingCharacter(std::string s)
 	}
 
 	return '_';
+}
+
+char firstNotRepeatingCharacter2(std::string s)
+{
+	std::unordered_map<char, int> map;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		char c = s.at(i);
+		bool found = map.find(c) != map.end();
+
+		if (found)
+		{
+			map[c] = INT_MAX;
+		}
+		else
+		{
+			map.insert(std::make_pair(c, i));
+		}
+	}
+
+	int idxMin = INT_MAX - 1;
+	char res = '_';
+
+	for (auto keyVal : map)
+	{
+		int index = keyVal.second;
+		char c = keyVal.first;
+
+		if (idxMin > index)
+		{
+			res = c;
+			idxMin = index;
+		}
+	}
+
+	return res;
 }
 
 int main(int argc, char const *argv[])
