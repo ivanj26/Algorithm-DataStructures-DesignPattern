be35a388e90379a4059e325d5986c0841fe042b3
ivanj26 <ivanjth26@gmail.com>
2022-01-04 07:49:58 +0700

    add simplify path

---
+++ Algorithm/String/simplifyPath.cpp
@@ -0,0 +1,79 @@
+#include <deque>
+#include <string>
+#include <iostream>
+
+enum Command {
+	BACK_TO_PARENT = '..',
+	CURR = '.'
+};
+
+void insertToCommands(std::string subStr, std::deque<std::string> &commands)
+{
+	switch (subStr) {
+		case Command::CURR:
+			// skip
+		break;
+		case Command::BACK_TO_PARENT:
+			if (!commands.empty()) {
+				commands.pop_back();
+			}
+		break;
+		default:
+			if (subStr.length() > 0) commands.push_back(subStr);
+	}
+}
+
+std::string createSimplePath(std::deque<std::string> &commands) {
+	std::string result = "/";
+    while (!commands.empty()) {
+        std::string c = commands.front();
+        commands.pop_front();
+        
+        result += result.size() == 1 ? c : ("/" + c);
+    }
+    
+    return result;
+}
+
+std::string simplifyPath(std::string path)
+{
+	std::string delim = "/";
+	std::deque<std::string> commands;
+
+	int start = 0;
+	int end = path.find(delim);
+
+	// consider. e.g. "/abc/bc"
+	// start = 0
+	// end = 0
+
+	// abc/bc
+	// start = 0 + 1 = 1
+	// end = 4
+	// result:
+	// 	substr = abc
+
+	while (end != -1) {
+		std::string subStr = path.substr(start, end - start);
+
+		insertToCommands(subStr, commands);
+
+		start = end + delim.size();
+		end = path.find(delim, start);
+	}
+
+	// handle last command
+	std::string lastCommand = path.substr(start, path.size() - start);
+	insertToCommands(lastCommand, commands);
+
+    return createSimplePath(commands);
+}
+
+int main(int argc, char const *argv[])
+{
+	std::string path = "";
+	std::string simpler = simplifyPath(path);
+
+	std::cout << simpler << std::endl;
+	return 0;
+}
