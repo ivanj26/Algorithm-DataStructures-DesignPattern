94fb9fdc4e48ef69ca387c2c1d83239e5d431fdf
ivanj26 <ivanjth26@gmail.com>
2022-01-13 13:51:35 +0700

    improve big sorting algorithm

--- Algorithm/Sort/bigSorting.cpp
+++ Algorithm/Sort/bigSorting.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 
 bool numericStringCompare(const string& s1, const string& s2) {
-    if (s1.length() == s2.length()) {
-        return s1 < s2;
-    }
+	if (s1.length() == s2.length()) {
+		int i = 0;
+		for (; i < s1.length() - 1; i++) {
+			int s1Int = s1[i] - '0';
+			int s2Int = s2[i] - '0';
 
-    return s1.length() < s2.length();
+			if (s1Int < s2Int) {
+				return true;
+			} else if (s1Int > s2Int) {
+				return false;
+			}
+		}
+
+		return s1[i] < s2[i];
+	}
+
+	return s1.length() < s2.length();
 }
 
 vector<string> bigSorting(vector<string> unsorted) {
-    std::sort(unsorted.begin(), unsorted.end(), numericStringCompare);
+	std::sort(unsorted.begin(), unsorted.end(), numericStringCompare);
 
-    return unsorted;
+	return unsorted;
 }
