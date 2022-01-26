398f6370cc5fefbf5c95f763b89bac0a875e0c10
ivanj26 <ivanjth26@gmail.com>
2022-01-27 06:58:18 +0700

    fixing quick sort, use left most as pivot

--- Algorithm/Sort/quicksort.cpp
+++ Algorithm/Sort/quicksort.cpp
@@ -1,63 +1,63 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *x, int *y)
-{
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
 int partition(int *arr, int low, int high)
 {
-	int pivot = (high + low) / 2;
+	// choose most left array value as pivot
+	int pivot = arr[low];
 
-	int i = low;
-	int j = high;
-
-	while (i <= j)
-	{
-		while (arr[i] < arr[pivot])
-		{
-			i++;
+	// loop while if low less than high
+	while (low < high) {
+		while (low < high && pivot <= arr[high]) {
+			high--;
 		}
 
-		while (arr[j] > arr[pivot])
-		{
-			j--;
+		arr[low] = arr[high];
+
+		while (low < high && pivot >= arr[low]) {
+			low++;
 		}
 
-		if (i <= j)
-		{
-			swap(&arr[i], &arr[j]);
-			i++;
-			j--;
-		}
+		arr[high] = arr[low];
 	}
 
-	return i;
+	arr[low] = pivot;
+
+	return low;
 }
 
-void quickSort(int *arr, int low = 0, int high)
+void quickSort(int *arr, int low = 0, int high = INT_MAX)
 {
-	if (low <= high)
-	{
-		int piv = partition(arr, low, high);
+	if (low < high) {
+		int mid = partition(arr, low, high);
+		quickSort(arr, low, mid);
+		quickSort(arr, mid + 1, high);
+	}
+}
 
-		if (low < piv - 1)
-		{
-			quickSort(arr, low, piv - 1);
-		}
-
-		if (high > piv)
-		{
-			quickSort(arr, piv, high);
-		}
+void printArray(int *arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << endl;
 	}
 }
 
 int main(int argc, const char **argv)
 {
+	int size = 5;
+	int *arr = new int[size];
 
+	arr[0] = 6;
+	arr[1] = 5;
+	arr[2] = -4;
+	arr[3] = 3;
+	arr[4] = 2;
+
+	// apply the quick sort
+	quickSort(arr, 0, size - 1);
+
+	// print the array
+	printArray(arr, size);
+
+	delete[] arr;
 	return 0;
 }
